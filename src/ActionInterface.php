662fa6b11e7759d57bdc35463d6a7c3ed30f41f3
Aurélien Schelcher <a.schelcher@ubitransports.com>
2015-12-18 22:55:58 +0100

    Add comment

--- src/ActionInterface.php
+++ src/ActionInterface.php
@@ -5,5 +5,11 @@ namespace Aschelch\Scenario;
 interface ActionInterface
 {
 
+    /**
+     * Execute the action using event's parameters
+     *
+     * @param  array Event params
+     * @return boolean true if the action has been executed, false otherwise
+     */
     public function execute($params);
 }
